6c1feeb4d1a4cc67cf59a311e6fe1cba0c2ab595
Glitch (flores-digitales) <none>
2020-10-10 22:16:40 +0000

    💖🗾 Updated with Glitch

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-/* TO DO:
+/* Maths for flowers: https://en.wikipedia.org/wiki/Rose_(mathematics)
+* TO DO:
 * maybe add the option to save as a pdf in order to print and colour by hand :D
-* animate flower
 
 Working on:
-
+* add a button to make a gift for someone 
 DONE:
 * remove sliders
 * d should take only one of the values from  [1,2,3,4,5,6,7,8,9]
@@ -13,21 +13,28 @@ DONE:
 * Canvas size should be equal to the screen size
 * If windows is resized canvas should be resized
 * on click create a random flower
+* rotate flower
+* add slider
+* add text to slider for customize of flower
 */
 
 function randomIntFromInterval(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let d, n, k, red, green, blue, cnvWeight, cnvHeight, flowerSize;
+
+let angle = 0;
+let d, n, k, red, green, blue, cnvWeight, cnvHeight, flowerSize, slider, helperText;
 
 function setup() {
   customCanvasSetup();
 
-  let button = createButton("Presiona aquí para que recibas tu flor de regalo");
-
   let cnv = createCanvas(cnvWidth, cnvHeight);
+  let button = createButton("Presiona aquí para que recibas tu flor de regalo");
   button.mousePressed(resetSketch);
+  helperText = createP('MODIFICA LA FLOR 👇').hide();
+  slider = createSlider(1, 9, 1).hide();
+  
 }
 
 function windowResized() {
@@ -37,22 +44,29 @@ function windowResized() {
 
 function customCanvasSetup() {
   cnvWidth = windowWidth * 0.9;
-  cnvHeight = windowHeight * 0.8;
+  cnvHeight = windowHeight * 0.7;
   flowerSize = Math.min(cnvWidth, cnvHeight) / 2.5;
 }
 
 function resetSketch() {
   d = 1;
   n = randomIntFromInterval(2, 7);
-  k = n / d;
   red = randomIntFromInterval(1, 255);
   green = randomIntFromInterval(1, 255);
   blue = randomIntFromInterval(1, 255);
+  
+  helperText.show()
+  slider.show()
 }
 
 function draw() {
+  angle += 1;
+  d = slider.value();
+  k = n / d;
   background(26, 26, 29);
   translate(width / 2, height / 2);
+  rotate(radians(angle));
+
   beginShape();
   noFill();
   stroke(red, green, blue);
